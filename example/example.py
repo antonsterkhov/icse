45e1724c124e1a4b190eba68ef018b13a5db3e4d
anton <capiton.82@mail.ru>
2017-07-23 20:24:58 +0300

    Example of using relay control

--- example/example.py
+++ example/example.py
@@ -1,13 +1,15 @@
-import time
 
 from src import icse
 
 relays = icse.ICES('/dev/ttyUSB0')
 
 relays.relay_on(1)
-time.sleep(2)
 
 relays.relay_off(1)
+
+relays.relays_on()
+
+relays.relays_off()
 
 
 
