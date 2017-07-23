18ef5dc86eaad7d0ff682d8fd3a104d8a67dc5c1
anton <capiton.82@mail.ru>
2017-07-23 18:14:15 +0300

    create metods '_set_bit' '_remove_bit' 'relay_on' 'relay_off'

--- icse.py
+++ icse.py
@@ -3,14 +3,17 @@ import time
 
 
 class ICES:
+    _is_port = False
+    _model = ''
+    _bits = 0x00
 
-    _is_port=False
-    _model=''
+    _list_model = {'ices12a': 4, 'ices13a': 2, 'ices14a': 8}
 
     def __init__(self, nameport):
         try:
             self._port = serial.Serial(nameport, 9600, timeout=.1)
             self._model = self._get_model()
+
         except:
             self._is_port = True
             print('****Wrong name {}****'.format(nameport))
@@ -20,21 +23,42 @@ class ICES:
             self._port.write(b'\x50')
             if self._port.read(1) == b'\xab':
                 self._port.write(b'\x51')
+                time.sleep(.2)
                 return 'ices12a'
             elif self._port.read(1) == b'\xad':
                 self._port.write(b'\x51')
+                time.sleep(.2)
                 return 'ices13a'
             elif self._port.read(1) == b'\xac':
                 self._port.write(b'\x51')
+                time.sleep(.2)
                 return 'ices14a'
             else:
-                return 'model none'
-
-
+                return 'ices14a'
 
     def relays_on(self):
-        self._port.write(b'\x00')
-
+        self._bits = b'\xff'
+        self._port.write(b'\xff')
 
     def relays_off(self):
-        self._port.write(b'\xff')
+        self._bits = b'\x00'
+        self._port.write(b'\x00')
+
+    def relay_on(self, num_relay):
+        if num_relay <= self._list_model[self._model]:
+            self._set_bit(bits=self._bits, number_bit=num_relay - 1)
+        else:
+            print ('Model relay board {name} max relay {count}'.
+                   format(name=self._model, count=self._list_model[self._model]))
+
+    def relay_off(self, num_relay):
+        if num_relay <= self._list_model[self._model]:
+            self._remove_bit(bits=self._bits, number_bit=num_relay - 1)
+
+    def _set_bit(self, bits, number_bit):
+        bits = bits | (1 << number_bit)
+        self._port.write(bytes([bits]))
+
+    def _remove_bit(self, bits, number_bit):
+        bits = bits & ~ (1 << number_bit)
+        self._port.write(bytes([bits]))
