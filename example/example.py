445d6d07b00c72f59e6fcc10b60cead24acee49a
anton <capiton.82@mail.ru>
2017-07-23 23:32:00 +0300

    Example of using relay control

--- example/example.py
+++ example/example.py
@@ -4,15 +4,16 @@ from src import icse
 relays = icse.ICES('/dev/ttyUSB0')
 
 # The number of relays for control depends on the model of the board.
+#
 
 time.sleep(1)
 
 while True:
 
-    for i in range(4):
-        relays.relay_on(i+1)
+    for i in range(1,4):
+        relays.relay_on(i)
         time.sleep(.5)
 
-    for i in range(4):
-        relays.relay_off(i+1)
+    for i in range(1,4):
+        relays.relay_off(i)
         time.sleep(.5)
